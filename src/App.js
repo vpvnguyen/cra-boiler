33ee96fb136af3aca88e76338efc0efed92ce61c
vpvnguyen <vpvnguyen@gmail.com>
2021-11-03 09:30:14 -0700

    removed unnecessary styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Switch, Route } from "react-router-dom";
-import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
